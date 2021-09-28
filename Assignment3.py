f99295da27664dd48b58d304544154b1bda605e8
khirthi <90829944+khirthi@users.noreply.github.com>
2021-09-28 21:57:32 +0530

    Add files via upload

---
+++ Assignment3.py
@@ -0,0 +1,11 @@
+NumList = []
+
+Number = int(input("Enter the total number of integers:"))
+for i in range(1, Number +1):
+    value = int(input("Please enter the values of %d elements: " %i))
+    NumList.append(value)
+
+print("\n Positive numbers in this list are: ")
+for j in range(Number):
+    if(NumList[j] >=0):
+        print(NumList[j], end= '\n')
